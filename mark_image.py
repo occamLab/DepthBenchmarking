17bc0ba24f7c62c3d7b7e140d28a573609368087
Kenta Burpee <kentaburpee@gmail.com>
2022-06-16 14:12:03 -0400

    Plot AR depth vs. MiDaS depth

--- mark_image.py
+++ mark_image.py
@@ -27,10 +27,15 @@ offset_y = data["intrinsics"][7]
 phone_fp = inv(pose) @ raw_fp
 camera_fp = np.array((phone_fp[0], -phone_fp[1], -phone_fp[2])).T
 
+print(camera_fp)
+
+ar_depths = []
 calc_projected_fp = []
 for row in camera_fp:
     pixel_x = row[0] * focal_length / row[2] + offset_x + 0.5
     pixel_y = row[1] * focal_length / row[2] + offset_y + 0.5
+    if 0 <= pixel_x <= frame.shape[0] and 0 <= pixel_y <= frame.shape[1]:
+        ar_depths.append(row[2] - focal_length)
     calc_projected_fp.append([pixel_x, pixel_y])
 
 calc_projected_fp = np.array(calc_projected_fp)
@@ -40,14 +45,24 @@ inverse_depth = np.array(\
     read_pfm("/Users/occamlab/Documents/ARPointCloud/output/frame.pfm")[0])
 midas_depth = np.reciprocal(inverse_depth.copy())
 
-plt.pcolor(midas_depth)
+plt.figure()
+plt.pcolor(midas_depth, cmap="PuBu")
 plt.colorbar()
-plt.show()
+plt.title("Visualization of MiDaS Depths")
 
+midas_depths_at_feature_points = []
 for row in projected_fp:
     pixel_x = row[0]
     pixel_y = row[1]
+    if 0 <= pixel_x <= frame.shape[0] and 0 <= pixel_y <= frame.shape[1]:
+        midas_depths_at_feature_points.append(midas_depth[pixel_x, pixel_y])
     cv.circle(frame, (pixel_x, pixel_y), 5, (51, 14, 247), -1)
-    
 
 cv.imwrite("output/featurepoints.jpg", frame)
+
+plt.figure()
+plt.scatter(ar_depths, midas_depths_at_feature_points)
+plt.xlabel("AR Depth")
+plt.ylabel("MiDaS Depth")
+
+plt.show()
