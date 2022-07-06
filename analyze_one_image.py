419ca956ce919e9fb6628da2eb9f8264f2dab34b
Kenta Burpee <kentaburpee@gmail.com>
2022-07-06 16:13:06 -0400

    Fix pixel indexing error and compare LiDAR vs feature points

--- analyze_one_image.py
+++ analyze_one_image.py
@@ -43,12 +43,12 @@ camera_fp = np.array((phone_fp[0], -phone_fp[1], -phone_fp[2])).T
 ar_depths = []
 calc_projected_fp = []
 for row in camera_fp:
-    pixel_x = row[0] * focal_length / row[2] + offset_x + 0.5
-    pixel_y = row[1] * focal_length / row[2] + offset_y + 0.5
-    if 0 <= round(pixel_x) < frame.shape[0] \
-        and 0 <= round(pixel_y) < frame.shape[1]:
+    pixel_col = row[0] * focal_length / row[2] + offset_x + 0.5
+    pixel_row = row[1] * focal_length / row[2] + offset_y + 0.5
+    if 0 <= round(pixel_col) < frame.shape[1] \
+        and 0 <= round(pixel_row) < frame.shape[0]:
         ar_depths.append(row[2])
-    calc_projected_fp.append([pixel_x, pixel_y])
+    calc_projected_fp.append([pixel_col, pixel_row])
 calc_projected_fp = np.array(calc_projected_fp)
 ar_depths = np.array(ar_depths)
 
@@ -92,15 +92,22 @@ plt.title("LiDAR Depth")
 
 # get MiDaS depth values from pixels with feature points
 midas_depths_at_feature_points = []
+lidar_depths_at_feature_points = []
+lidar_confidence_at_feature_points = []
 for row in projected_fp:
-    pixel_x = row[0]
-    pixel_y = row[1]
-    if 0 <= pixel_x < frame.shape[0] and 0 <= pixel_y < frame.shape[1]:
-        midas_depths_at_feature_points.append(midas_depth[pixel_x, pixel_y])
-        
-    cv.circle(frame, (pixel_x, pixel_y), 5, (51, 14, 247), -1)
+    pixel_col = row[0]
+    pixel_row = row[1]
+    if 0 <= pixel_col < frame.shape[1] and 0 <= pixel_row < frame.shape[0]:
+        midas_depths_at_feature_points.append(midas_depth[pixel_row, pixel_col])
+        lidar_depths_at_feature_points.append(lidar_depth[round(pixel_row / 7.5), round(pixel_col / 7.5)])
+        lidar_confidence_at_feature_points.append(lidar_confidence[round(pixel_row / 7.5), round(pixel_col / 7.5)])
+        cv.circle(frame, (pixel_col, pixel_row), 5, (51, 14, 247), -1)
+        cv.putText(frame, str(len(midas_depths_at_feature_points)), \
+            (pixel_col, pixel_row), cv.FONT_HERSHEY_COMPLEX, 1, (51, 14, 247))
 
 midas_depths_at_feature_points = np.array(midas_depths_at_feature_points)
+lidar_depths_at_feature_points = np.array(lidar_depths_at_feature_points)
+lidar_confidence_at_feature_points = np.array(lidar_confidence_at_feature_points)
 
 # draw circles on the input image
 cv.imwrite("output/featurepoints.jpg", frame)
@@ -181,6 +188,24 @@ less_five_corr_map = ((less_five_lidar - np.nanmean(less_five_lidar)) / \
 plt.pcolor(less_five_corr_map, cmap="RdYlGn")
 plt.colorbar()
 plt.title("Corrleation between Close Distance LiDAR and MiDaS")
+
+plt.figure()
+plt.scatter(ar_depths, lidar_depths_at_feature_points)
+plt.xlabel("Feature Points")
+plt.ylabel("LiDAR")
+plt.title("Feature Points vs All LiDAR")
+for i in range(ar_depths.size):
+    plt.annotate(str(i), (ar_depths[i], lidar_depths_at_feature_points[i]))
+
+plt.figure()
+plt.scatter(ar_depths[lidar_confidence_at_feature_points==2], \
+    lidar_depths_at_feature_points[lidar_confidence_at_feature_points==2], c="g")
+plt.xlabel("Feature Points")
+plt.ylabel("High Confidence LiDAR")
+plt.title("Feature Points vs High Confidence LiDAR")
+for i in range(ar_depths.size):
+    if lidar_confidence_at_feature_points[i] == 2:
+        plt.annotate(str(i), (ar_depths[i], lidar_depths_at_feature_points[i]))
 
 # show the plots
 plt.show()
